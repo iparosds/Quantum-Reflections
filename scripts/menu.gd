307c9f28563428f01dfbe639feb20fc4911d40a7
iBerserker89 <lucianof.barros89@gmail.com>
2024-11-16 23:16:02 -0300

    incluindo som e fx

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,15 +1,20 @@
 class_name Menu
 extends Control
 
-@onready var new_game_button =preload("res://scenes/game.tscn") as PackedScene
+@onready var new_game_button =$MarginContainer/VBoxContainer/New_Game as Button
 @onready var settings = $MarginContainer/VBoxContainer/Settings as Button
 @onready var rewards = $MarginContainer/VBoxContainer/Rewards as Button
 @onready var credits = $MarginContainer/VBoxContainer/Credits as Button
 @onready var start = $"MarginContainer/VBoxContainer/START!" as Button
 @onready var quit = $MarginContainer/VBoxContainer/Quit as Button
+@onready var start_game =preload("res://scenes/game.tscn") as PackedScene
+
+func _ready():
+	$menu_music.play()
+	new_game_button.grab_focus()
 
 func _on_new_game_pressed() -> void:
-	get_tree().change_scene_to_packed(new_game_button)
+	get_tree().change_scene_to_packed(start_game)
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/settings_menu.tscn")
@@ -25,3 +30,11 @@ func _on_start_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("move_up"):
+		$up_sound.play()
+	if Input.is_action_just_pressed("move_down"):
+		$down_sound.play()
+	if Input.is_action_just_pressed("enter"):
+		$select_sound.play()
--- scripts/settings_menu.gd
+++ scripts/settings_menu.gd
@@ -1,5 +1,13 @@
 extends Control
 
+@onready var controls =$MarginContainer/VBoxContainer/Controls as Button
+@onready var volume = $MarginContainer/VBoxContainer/Volume as Button
+@onready var back = $MarginContainer/VBoxContainer/Back as Button
+
+func _ready():
+	$menu_music.play()
+	controls.grab_focus()
+
 func _on_controls_pressed() -> void:
 	pass # Replace with function body.
 
@@ -8,3 +16,14 @@ func _on_volume_pressed() -> void:
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("move_up"):
+		$up_sound.play()
+	if Input.is_action_just_pressed("move_down"):
+		$down_sound.play()
+	if Input.is_action_just_pressed("enter"):
+		$select_sound.play()
+	if Input.is_action_just_pressed("back"):
+		$back_sound.play()
+		get_tree().change_scene_to_file("res://scenes/menu.tscn")
