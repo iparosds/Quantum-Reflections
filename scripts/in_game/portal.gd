acb4dc795fb013868dbe94b68cf886058a75acd2
iBerserker89 <lucianof.barros89@gmail.com>
2025-08-22 14:34:11 -0300

    fazer tela de pause [issue: #27]

--- scripts/in_game/portal.gd
+++ scripts/in_game/portal.gd
@@ -6,6 +6,6 @@ var level
 func _on_body_entered(body):
 	if Singleton.level.portal_active == true:
 		if body.has_method("is_player"):
-			level.win()
+			Singleton.level.win()
 		if body.has_method("on_portal"):
 			body.on_portal()
