d58f09596f9a0234870bb914ed415d546145dbaf
iBerserker89 <lucianof.barros89@gmail.com>
2025-10-22 17:45:12 -0300

    criar lógica básica para arma 2 ricochetear

--- scripts/game/bullet_2.gd
+++ scripts/game/bullet_2.gd
@@ -1,34 +1,52 @@
 class_name Bullet2 extends Area2D
 
-const RANGE = 300
+const RANGE : float= 200.0
+const MAX_BOUNCES : int = 3
+
 @onready var projectile: AnimatedSprite2D = %Projectile
-var target
-var direction
-var travelled_distance = 0
-var move_speed = 200
+
+var target : Node2D
+var direction: Vector2
+var travelled_distance : float= 0.0
+var move_speed : float = 200.0
 var damage_multiplier: float = 1.0
+var bounce_count : int = 0
 
 
 func _ready():
-	direction = global_position.direction_to(target.global_position)
+	direction = global_position.direction_to(target.global_position).normalized()
+
 
 func _physics_process(delta):
 	if travelled_distance > RANGE:
 		queue_free()
+		return
 	if target == null:
 		queue_free()
+		return
 	else:
 		if move_speed != 0:
-			move_speed += 20
+			move_speed += 5
 			position += direction * move_speed * delta
 			travelled_distance += move_speed * delta
-			projectile.rotate(0.7)
+			projectile.rotate(0.5)
+
 
 func _on_body_entered(body):
 	if body.has_method("take_damage"):
-		# -1.0 : "calcula o base pela velocidade", e enviamos o multiplicador
 		body.take_damage(-1.0, damage_multiplier)
-	queue_free()
+		if bounce_count < MAX_BOUNCES:
+			var diff_position = global_position - body.global_position
+			if diff_position.length() > 0.0001:
+				direction = direction.bounce(diff_position.normalized()).normalized()
+				bounce_count += 1
+			else:
+				direction = -direction
+				bounce_count += 1
+		else:
+			queue_free()
+	else:
+		queue_free()
 
 
 func set_damage_multiplier(multiplier: float) -> void:
