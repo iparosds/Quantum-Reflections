589070e4df3772c031fbdd2917de0bdfebc76340
iBerserker89 <lucianof.barros89@gmail.com>
2025-10-23 12:56:33 -0300

    adicionar chance de 30% de a arma 2 ricochetear

--- scripts/game/bullet_2.gd
+++ scripts/game/bullet_2.gd
@@ -11,9 +11,13 @@ var travelled_distance : float= 0.0
 var move_speed : float = 200.0
 var damage_multiplier: float = 1.0
 var bounce_count : int = 0
+var bounce_chance: float = 0.30
+var can_bounce: bool = false
 
 
 func _ready():
+	randomize()
+	can_bounce = randf() < bounce_chance
 	direction = global_position.direction_to(target.global_position).normalized()
 
 
@@ -35,7 +39,7 @@ func _physics_process(delta):
 func _on_body_entered(body):
 	if body.has_method("take_damage"):
 		body.take_damage(-1.0, damage_multiplier)
-		if bounce_count < MAX_BOUNCES:
+		if can_bounce and bounce_count < MAX_BOUNCES:
 			var diff_position = global_position - body.global_position
 			if diff_position.length() > 0.0001:
 				direction = direction.bounce(diff_position.normalized()).normalized()
